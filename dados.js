8d30c73983f5b3815ddfb2280cd1ba04c1a20f73
italoGGWP <italohiquee@gmail.com>
2024-09-10 08:33:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,48 @@
+let dados = [
+
+    {
+        titulo: "Pelé",
+        descricao: "Pelé, nome artístico de Edson Arantes do Nascimento, é considerado por muitos o maior jogador de futebol de todos os tempos. Sua habilidade excepcional, dribles mágicos e gols memoráveis o consagraram como um ícone mundial do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Pelé",
+        nascimento: 1940,
+        falecimento: 2022,
+        profissoes: "jogador de futebol",
+        tags: "fut copa 1940 2022",
+    },
+    {
+        titulo: "Ayrton Senna",
+        descricao: "Ayrton Senna foi um piloto de Fórmula 1 brasileiro, tricampeão mundial. Sua paixão pelas corridas e sua habilidade o tornaram um ícone do esporte a motor.",
+        link: "https://pt.wikipedia.org/wiki/Ayrton_Senna",
+        nascimento: 1960,
+        falecimento: 1994,
+        profissoes: "piloto de Fórmula 1",
+        tags: "formula campeão acidente 1960 1994",
+    },
+    {
+        titulo: "Chico Buarque",
+        descricao: "Cantor, compositor e escritor, Chico Buarque é uma das maiores figuras da música popular brasileira, com canções que abordam temas sociais e políticos.",
+        link: "https://pt.wikipedia.org/wiki/Chico_Buarque",
+        nascimento: 1944,
+        falecimento: "esta vivo ainda",
+        profissoes: "cantor",
+        tags: "1944 musico antigo",
+    },
+    {
+        titulo: "Carlos Drummond de Andrade",
+        descricao: "Um dos maiores poetas brasileiros, conhecido por sua poesia social e intimista.",
+        link: "https://pt.wikipedia.org/wiki/Carlos_Drummond_de_Andrade",
+        nascimento: 1902,
+        falecimento: 1987,
+        profissoes: "poeta",
+        tags: "poeta 1902 1987",
+    },
+    {
+        titulo: "Hebe Camargo",
+        descricao: "Apresentadora de televisão brasileira, conhecida por sua simpatia e programas de grande audiência.",
+        link: "https://pt.wikipedia.org/wiki/Hebe_Camargo",
+        nascimento: 1929,
+        falecimento: 2012,
+        profissoes: "apresentadora de televisão",
+        tags: "1929 2012 apresentador tv",
+    }
+]
